522c9ad8146841e1ea99cf62b6000889a99efcae
Shadhyb <pietro.rimoli89@gmail.com>
2022-11-02 17:00:17 +0100

    js2

---
+++ M1/W2/D2/assets/js/D2.js
@@ -0,0 +1,86 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Se sei in difficoltà puoi chiedere aiuto a un Teaching Assistant
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per farlo puoi utilizzare il terminale Bash, quello di VSCode o quello del tuo sistema operativo (se utilizzi macOS o Linux)
+*/
+
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+*/
+
+function esercizio1(){
+  alert('sono i tipi di dato che JS accetta e possono essere numerici, alfabetici, booleani, undefined e null. Booleani sono valori true o false, null è un valore intenzionalmente assente, undefined è un dato senza valore')
+}
+
+
+
+/* ESERCIZIO 2
+ Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
+*/
+var nome =('Pietro')
+console.log (nome)
+function esercizio2(){
+  alert ('Pietro')
+}
+
+/* ESERCIZIO 3
+ Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+
+var x = 12
+var y = 20
+
+console.dir (x + y)
+function esercizio3(){
+  alert (32)
+}
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+
+var x = 12
+function esercizio4(){
+  alert (x=12)
+}
+
+/* ESERCIZIO 5
+  Riassegna un nuovo valore alla variabile "name" già esistente: il tuo cognome.
+  Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
+*/
+var nome = ('Rimoli')
+//const costante =('Rimoli') /*togliere il commento per verificare
+console.dir(nome)
+function esercizio5(){
+  alert(nome)
+
+}
+
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+
+var numero = 4
+console.dir(4 + x)
+function esercizio6(){
+alert(numero)
+}
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+*/
+var nome1 = ('john')
+var nome2 = ('John')
+var john = (nome1 != nome2)
+console.log (john)
+function esercizio7(){
+  alert (john)
+}
+
+
+
---
+++ M1/W2/D3/assets/js/D3.js
@@ -0,0 +1,220 @@
+/* ESERCIZIO 1
+ Scrivi un algoritmo per trovare il più grande tra due numeri interi.
+*/
+let num1 = 5
+let num2 = 3
+let maggiore = (num2>num1)
+console.log(maggiore)
+function esercizio1(){
+  alert (maggiore)
+}
+
+/* ESERCIZIO 2
+  Scrivi un algoritmo che mostri "not equal" in console se un numero intero fornito è diverso da 5.
+*/
+
+let numeroGiusto = 5
+let numeroSbagliato = 3
+let diff = (numeroGiusto === numeroSbagliato)
+console.dir(diff)
+function esercizio2(){
+  alert(diff)
+}
+/* ESERCIZIO 3
+  Scrivi un algoritmo che mostri "divisibile per 5" in console se un numero fornito è perfettamente divisibile per 5 (suggerimento: cerca l'operatore modulo su un motore di ricerca)
+*/
+let risultato = (4%5)
+console.dir(risultato)
+function esercizio3(){
+  alert(risultato)
+}
+
+/* ESERCIZIO 4
+  Scrivi un algoritmo per verificare che, dati due numeri interi, il valore di uno di essi sia 8 oppure se la loro addizione/sottrazione sia uguale a 8.
+*/
+
+let x = 4
+let y = 3
+let somma =(x+y)
+function esercizio4(){
+  if(somma==8){alert('giusto')}
+  else{alert('sbagliato')}
+}
+/* ESERCIZIO 5
+  Stai lavorando su un sito di e-commerce. Stai salvando il saldo totale del carrello dell'utente in una variabile "totalShoppingCart".
+  C'è una promozione in corso: se il totale del carrello supera 50, l'utente ha diritto alla spedizione gratuita (altrimenti la spedizione ha un costo fisso pari a 10).
+  Crea un algoritmo che determini l'ammontare totale che deve essere addebitato all'utente per il checkout.
+  
+*/
+
+
+let smartphone = {
+  marca:'apple',
+  prezzo: 10,
+  aggiunte: [
+      {
+          nome:'cover',
+          prezzo:20
+      },
+      {
+          nome:'assicurazione',
+          prezzo:30
+      },
+      {
+          nome:'cuffie',
+          prezzo:40
+      },
+  ],
+  mostraPrezzo: function(){//metodo
+      console.log('Spedizione gratuita con 50 € di spesa')
+  }
+}
+
+console.log(smartphone.prezzo)
+console.log(smartphone.aggiunte[3])
+smartphone.mostraPrezzo()
+if (smartphone.prezzo <50) {console.log('spedizione al costo di 10€')}
+else{console.log('Complimenti, la spedizione è gratuita')}
+
+
+
+
+/* ESERCIZIO 6
+  Stai lavorando su un sito di e-commerce. Oggi è il Black Friday e viene applicato il 20% su ogni prodotto.
+  Modifica la risposta precedente includendo questa nuova promozione nell'algoritmo, determinando come prima se le spedizioni sono gratuite oppure no e e calcolando il totale.
+*/
+let smartphone2 = {
+  marca:'apple',
+  prezzo: 10,
+  aggiunte: [
+      {
+          nome:'cover',
+          prezzo:20,
+          sconto:6
+      },
+      {
+          nome:'assicurazione',
+          prezzo:30,
+          sconto:6
+      },
+      {
+          nome:'cuffie',
+          prezzo:40,
+          sconto:6
+      },
+  ],
+  mostraPrezzo: function(){//metodo
+      console.log('Spedizione gratuita con 50 € di spesa')
+  }
+}
+
+console.log(smartphone2.prezzo)
+console.log(smartphone2.aggiunte[3])
+console.dir(10 % 6)
+smartphone2.mostraPrezzo()
+if (smartphone.prezzo.sconto <50) {console.log('spedizione al costo di 10€')}
+else{console.log('Complimenti, la spedizione è gratuita')}
+
+
+
+/* ESERCIZIO 7
+  Crea tre variabili, e assegna un valore numerico a ciascuna di esse.
+  Utilizzando un blocco condizionale, crea un algoritmo per ordinarle secondo il loro valore, dal più alto al più basso.
+  Alla fine mostra il risultato in console.
+*/
+
+let a=5
+let b=7
+let c=20
+let z=(c>b>a)
+
+console.log(z)
+function esercizio7(){
+  alert (z)
+}
+
+
+/* ESERCIZIO 8
+  Crea un algoritmo per verificare che un valore fornito sia un numero oppure no (suggerimento: cerca su un motore di ricerca "typeof").
+*/
+
+let parola=('john')
+typeof('john')
+console.log(parola)
+
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 9
+  Crea un algoritmo per controllare se un numero fornito sia pari o dispari (suggerimento: cerca l'operatore modulo su un motore di ricerca)
+*/
+let f = 3
+
+let risult = (f % 2)
+console.log (risult)
+function esercizio9(){
+  alert(risult)
+}
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 10
+  Modifica la logica del seguente algoritmo in modo che mostri in console il messaggio corretto in ogni circostanza.*/
+  let val = 7;
+  val = 4;
+  val +=11
+  if (val < 10) {
+      console.log("Meno di 10");
+    } else if (val < 5) {
+      console.log("Meno di 5");
+    } else {
+      console.log("Uguale a 10 o maggiore");
+    }
+
+
+
+
+/* ESERCIZIO 11
+  Fornito il seguente oggetto, scrivi del codice per aggiungere una proprietà "city", il cui valore sarà "Toronto".
+*/
+
+const me = {
+  name: 'John',
+  lastName: 'Doe',
+  skills: ['javascript', 'html', 'css'],
+  city: 'Toronto'
+}
+console.log(me)
+
+
+/* ESERCIZIO 12
+  Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere la proprietà "lastName".
+*/
+const me2 = {
+  name: 'John',
+  lastName: 'Doe',
+  skills: ['javascript', 'html', 'css'],
+  city: 'Toronto'
+}
+delete lastName
+console.log(me2)
+
+
+/* ESERCIZIO 13
+  Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere l'ultimo elemento della proprietà "skills".
+*/
+
+
+
+
+/* ESERCIZIO 14
+  Scrivi del codice per creare un array inizialmente vuoto. Riempilo successivamente con i numeri da 1 a 10.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 15
+  Scrivi del codice per sostituire l'ultimo elemento dell'array, ovvero il valore 10, con il valore 100.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
