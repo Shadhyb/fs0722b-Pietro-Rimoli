829ef3bf64b6d6577d7573e6620102f85f5bb0bd
Shadhyb <pietro.rimoli89@gmail.com>
2022-11-07 18:09:08 +0100

    js2

--- M1/W2/progetto/assets/script/script.js
+++ M1/W2/progetto/assets/script/script.js
@@ -18,7 +18,10 @@ function percentage(){
     document.getElementById('ops').value /=100 
 }
 
+function esercizi(){
+document.getElementsByClassName('uguale tasto') = ('document.getElementById("ops").value = eval(document.getElementById('ops').value)' )
 
+//Lu ti voglio stupire sta volta, quella funzione che volevo tanto fare probabilmente la faccio..intanto ho pushato alle 17.48..se vedi commit successivi ci sono riuscito..magari ho sbagliato tutto
 
  
 
