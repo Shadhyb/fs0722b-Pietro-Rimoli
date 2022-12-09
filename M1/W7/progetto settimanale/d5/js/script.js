2f99f8b21e9766a4137f7db319efb3838a532fea
Shadhyb <pietro.rimoli89@gmail.com>
2022-12-09 17:13:59 +0100

    fix immagini

--- M1/W7/progetto settimanale/d5/js/script.js
+++ M1/W7/progetto settimanale/d5/js/script.js
@@ -5,12 +5,12 @@ fetch ('users.json').then(function (response){
 classe = json; 
 console.log(classe)
 
-document.querySelector('#imgUser1').innerHTML = `<img class="bg-secondary bg-opacity-75 border rounded-circle" src ="img/male.png"></img>`
+document.querySelector('#imgUser1').innerHTML = `<img class="bg-secondary bg-opacity-75 border rounded-circle" src =${'img/male.png'}></img>`
 document.querySelector('#user1').innerHTML = classe[0].firstName + " " + classe[0].lastName + " " ;
 document.querySelector('#emailUser1').innerHTML = classe[0].email;
 document.querySelector('#user2').innerHTML = classe[1].firstName + " " + classe[1].lastName;
 document.querySelector('#emailUser2').innerHTML = classe[1].email;
-document.querySelector('#imgUser2').innerHTML = `<img class="bg-secondary bg-opacity-75 border rounded-circle" src ="img/female.png"></img>`
+document.querySelector('#imgUser2').innerHTML = `<img class="bg-secondary bg-opacity-75 border rounded-circle" src ="${'img/female.png'}"></img>`
 
 }).catch(function(err){
     console.log ('fetch problem: '+ err.message);
