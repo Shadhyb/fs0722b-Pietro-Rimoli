1e6d36a6f6becddf248b39b411c34a2083aa33c2
Shadhyb <pietro.rimoli89@gmail.com>
2022-12-19 16:25:11 +0100

    typescript

---
+++ M1/W8/D1/typescript/typescript.js
@@ -0,0 +1,20 @@
+var user1 = 50;
+var user2 = 40;
+console.log("utente1 ha scelto: " + user1);
+console.log("utente2 ha scelto: " + user2);
+var bottone = document.getElementById('winner');
+var numeroVincente = document.getElementById('div');
+var random = (Math.floor(Math.random() * 100));
+console.log("numero vincente: " + random);
+var div = document.createElement('div').innerHTML = "the winner is ".concat(random);
+bottone.append(div);
+if (random <= user1) {
+    console.log("il vincitore è user1");
+    document.body.appendChild(div);
+}
+else if (random <= user2) {
+    console.log("il vincitore è user2");
+}
+else {
+    console.log("nessun vincitore");
+}
---
+++ M1/W8/D1/typescript/typescript.ts
@@ -0,0 +1,15 @@
+let user1 = 50;
+let user2 = 40
+console.log("utente1 ha scelto: " + user1)
+console.log("utente2 ha scelto: " + user2)
+let bottone =document.getElementById('winner')
+let numeroVincente : any = document.getElementById('div');
+let random = (Math.floor(Math.random()*100))
+console.log("numero vincente: " + random)
+let div = document.createElement('div').innerHTML =`the winner is ${random}`
+bottone.append(div)
+if (random <= user1){
+    console.log("il vincitore è user1")
+    
+}else if (random <= user2){console.log("il vincitore è user2")
+}else{console.log("nessun vincitore")}
