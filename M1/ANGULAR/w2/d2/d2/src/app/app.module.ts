3f9c185a91b3e24c4b32b355116109036fe4ea43
Shadhyb <pietro.rimoli89@gmail.com>
2023-01-17 17:08:26 +0100

    reactiveForm

--- M1/ANGULAR/w2/d2/d2/src/app/app.module.ts
+++ M1/ANGULAR/w2/d2/d2/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { ReactiveComponent } from './component/reactive/reactive.component';
 import { AppComponent } from './app.component';
 import { TdFormComponent } from './td-form/td-form.component';
+import { ReactiveFormsModule } from '@angular/forms'
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TdFormComponent
+    TdFormComponent,
+    ReactiveComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
